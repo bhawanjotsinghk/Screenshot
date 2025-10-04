80fe7f6587df66d576f84100a093e3445e65158f
Bhawanjot Singh <bhawanjotsinghk@gmail.com>
2025-10-04 17:06:35 -0300

    Initial Commit

---
+++ ScreenshotTests/ScreenshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotTests.swift
+//  ScreenshotTests
+//
+//  Created by Bhawanjot Singh Kooner on 2025-10-04.
+//
+
+import Testing
+@testable import Screenshot
+
+struct ScreenshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
